13df521bd28a5680f742102970a9e632f6394c87
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2025-03-29 14:53:37 +0700

    Calculator By Euro

---
+++ Week 2 ( JavaScript )/Calculator using HTML, CSS & JavaScript/script.js
@@ -0,0 +1,45 @@
+
+
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let currentString = "";
+let isResultDisplayed = false;
+
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const btnValue = e.target.innerHTML;
+        console.log(btnValue);
+
+        // =: Equal
+        if (btnValue === '=') {
+            try {
+                currentString = eval(currentString).toString();
+                input.value = currentString;
+                isResultDisplayed = true;
+            } catch (error) {
+                input.value = "Error";
+                currentString = "";
+            }
+            return;
+        }
+
+        // AC: Clear Display
+        if (btnValue === 'AC') {
+            currentString = "";
+            input.value = "";
+            return;
+        } 
+
+        // DEL: Delete 1 at last
+        if (btnValue === 'DEL') {
+            currentString = currentString.slice(0, -1);
+            input.value = currentString;
+            return;
+        }
+
+        currentString += btnValue;
+        input.value = currentString;
+    });
+});
